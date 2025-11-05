13bedca99a9d09316310ee18d087731671fac55c
iCxts <mrcatwastaken@gmail.com>
2025-11-05 14:00:05 +0700

    final i think

---
+++ testfile.py
@@ -0,0 +1,13 @@
+#def bitshiftleft(integer, shift): return integer * (2 ** shift)
+#print(bitshiftleft(2, 2))
+#print(2 << 2)
+#
+#def bitshiftright(integer, shift): return integer // (2 ** shift)
+#print(bitshiftright(333, 2))
+#print(333 >> 2)
+
+#def is_int_conv(string): return string.isdigit()
+#print(is_int_conv("123") == True)
+
+
+
